e620208341091297a18912fdad68959b18d3d929
teusebio <teusebio@student.21-school.ru>
2022-05-21 08:33:04 +0300

    day04 ex00 fix url

--- day04/ex00/d04/d04/urls.py
+++ day04/ex00/d04/d04/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('ex00', include('ex00.urls')),
+    path('ex00/', include('ex00.urls')),
     path('admin/', admin.site.urls),
 ]
