1fc84ca4b90e3bc5469185c105a4558e6af5f74d
teusebio <teusebio@student.21-school.ru>
2022-05-24 16:17:41 +0300

    day05 ex01 fix

--- day05/d05/ex01/models.py
+++ day05/d05/ex01/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Movies(models.Model):
-    title = models.CharField(max_length=64, unique=True, null=False)
     episode_nb = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=64, unique=True, null=False)
     opening_crawl = models.TextField()
     director = models.CharField(max_length=32, null=False)
     producer = models.CharField(max_length=128, null=False)
